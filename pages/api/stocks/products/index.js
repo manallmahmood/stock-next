af3fedc11fef4dfd4448ddb43a51bdf116b64c60
Manal <manallmahmood.github.io>
2023-02-26 21:42:00 +0700

    init

--- pages/api/stocks/products/index.js
+++ pages/api/stocks/products/index.js
@@ -1,10 +1,30 @@
-import path from 'path';
-import { promises as fs } from 'fs';
+import { connect, model, models, Schema } from "mongoose";
+const connectionString =
+  "mongodb+srv://manallmahmood:stupid123@cluster0.y1xgwpp.mongodb.net/stocks";
+
 export default async function handler(req, res) {
-  //Find the absolute path of the json directory
-  const jsonDirectory = path.join(process.cwd(), 'data');
-  //Read the json data file data.json
-  const fileContents = await fs.readFile(jsonDirectory + '/products.json', 'utf8');
-  //Return the content of the data file in json format 
-  res.status(200).json(JSON.parse(fileContents));
+  await connect(connectionString);
+  console.log("req.method: ", req.method);
+
+  if (req.method === "GET") {
+    const docs = await Products.find();
+    res.status(200).json(docs);
+  } else if (req.method === "POST") {
+    console.log(req.body);
+    // res.status(200).json(req.body);
+    const doc = await Products.create(req.body);
+    res.status(201).json(doc);
+  } else {
+    res.setHeader("Allow", ["GET", "POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
 }
+
+const productSchema = new Schema({
+  code: String,
+  name: String,
+  price: { type: Number, float: true },
+});
+
+console.log("Mongoose Models", models);
+const Products = models?.products || model("products", productSchema);
